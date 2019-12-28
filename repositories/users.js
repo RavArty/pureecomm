f16491e1f8cc2d93242e5c1f1353e394c1d67efb
Ravshan <ravkart@gmail.com>
2019-12-28 13:16:56 -0800

    added getAll()

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,19 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8'
+    });
+
+    console.log(contents);
+  }
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  await repo.getAll();
+};
+
+test();
