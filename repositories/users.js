8170ae86db5cd18e1954625ef7639fb16180141a
Ravshan <ravkart@gmail.com>
2019-12-28 13:32:44 -0800

    added create func

--- repositories/users.js
+++ repositories/users.js
@@ -20,11 +20,21 @@ class UsersRepository {
       })
     );
   }
+
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.getAll();
+
+  await repo.create({ email: 'email', password: 'pass' });
+  const users = await repo.getAll();
+
+  console.log(users);
 };
 
 test();
