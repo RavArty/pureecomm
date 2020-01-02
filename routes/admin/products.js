e737ef30af02c42b8ae745f3e9f8ebe1de67cd21
Ravshan <ravkart@gmail.com>
2020-01-01 16:48:58 -0800

    created products repository

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const productsRepo = require('../../repositories/products');
+
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
