31e282502eaab2deebe507787f89845847399e0d
Ravshan <ravkart@gmail.com>
2020-01-01 17:15:35 -0800

    validation for title and price

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators ');
+
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
+});
+
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+  res.send('submitted');
 });
 
 module.exports = router;
