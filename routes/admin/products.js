b82b4d316f0a1d9963ca3b1f46a7ba21408c8951
Ravshan <ravkart@gmail.com>
2020-01-03 12:10:56 -0800

    handling edit post

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -20,6 +20,16 @@ router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+
+  if (!product) {
+    return res.send('Product not found');
+  }
+
+  res.send(productsEditTemplate({ product }));
+});
+
 router.post(
   '/admin/products/new',
   requireAuth,
@@ -35,7 +45,7 @@ router.post(
   }
 );
 
-router.get('/admin/products/:id/edit', async (req, res) => {
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
 
   if (!product) {
@@ -43,5 +53,25 @@ router.get('/admin/products/:id/edit', async (req, res) => {
   }
   res.send(productsNewTemplate({ product }));
 });
+
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate),
+  async (req, res) => {
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Could not find item');
+    }
+    res.redirect('/admin/products');
+  }
+);
 
 module.exports = router;
