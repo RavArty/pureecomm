fe386acac53110bd91031d238c9c748869ac165e
Ravshan <ravkart@gmail.com>
2020-01-02 20:37:41 -0800

    fixed middleware bug in products/new

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const multer = require('multer');
+const { validationResult } = require('express-validator');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const { requireTitle, requirePrice } = require('./validators ');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -11,8 +15,22 @@ router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-  res.send('submitted');
-});
+router.post(
+  '/admin/products/new',
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.send('submitted');
+  }
+);
 
 module.exports = router;
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,9 +4,11 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <form method='POST'>
+      <form method='POST' enctype='multipart/form-data'>
         <input placeholder='Title' name='title'/>
+        ${getError(erros, 'title')}
         <input placeholder='Price' name='price'/>
+        ${getError(erros, 'price')}
         <input type='file' name='image'/>
         <button>Submit</button>
       </form>
