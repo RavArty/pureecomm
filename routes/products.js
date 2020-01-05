78d24bd60a456ff8f3378e3ff41c8e0d40a93a3d
Ravshan <ravkart@gmail.com>
2020-01-04 18:28:27 -0800

    passing products to main page

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const productsRepo = require('../repositories/products');
 const router = express.Router();
+const productsIndexTemplate = require('../views/products/index');
 
 router.get('/', async (req, res) => {
-  res.send('products');
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
 });
 
 module.exports = router;
