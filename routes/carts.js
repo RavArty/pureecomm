3c5a01e2426007d17195c249e8999604c2295482
Ravshan <ravkart@gmail.com>
2020-01-04 19:18:58 -0800

    adding items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -10,6 +10,18 @@ router.post('/cart/products', async (req, res) => {
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
+
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
+
+  res.send('added');
 });
 
 module.exports = router;
