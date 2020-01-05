71d07fa0000e5146661de6c086f3f46df4ea3e87
Ravshan <ravkart@gmail.com>
2020-01-04 19:06:02 -0800

    creating a cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,15 @@
 const express = require('express');
-
+const cartsRepo = require('../repositories/carts');
 const router = express.Router();
+
+router.post('/cart/products', async (req, res) => {
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+});
 
 module.exports = router;
